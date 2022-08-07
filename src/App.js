c57bc10d8da9fef32001cd386f568f1b1adccd48
Igor Lukjanov <lukjanov.igr@gmail.com>
2022-08-07 13:09:34 +0300

    Add favicon and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import { NotFound } from './pages/index';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
-      <h2>Hello</h2>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" />
+        <Route path="about" />
+        <Route path="order-summary" />
+
+        <Route path="products">
+          <Route index />
+          <Route path="jackets" />
+          <Route path="shoes" />
+        </Route>
+
+        <Route path="users">
+          <Route path=":userId" />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
